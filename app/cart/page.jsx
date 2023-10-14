9a3b89105ccdd21413fa5049d512ca6c0eb9932e
javanance <javanance@gmail.com>
2023-10-14 14:58:13 +0900

    next neon connection

--- app/cart/page.jsx
+++ app/cart/page.jsx
@@ -1,13 +1,19 @@
 import { sql } from "@vercel/postgres";
 
-export default async function Cart({ params }) {
-  const { rows } = await sql`SELECT * from pets`;
+export default async function Cart() {
+  const { rows } = await sql`SELECT * FROM pets`;
 
   return (
-    <div>
-      {rows.map((row) => (
-        <div key={row.id}>{row.name}</div>
-      ))}
-    </div>
+    <>
+      <h1>pets</h1>
+      <div>
+        {rows.map((row) => (
+          <div key={row.id}>
+            {row.name} {"_"}
+            {row.email} {"_"}
+          </div>
+        ))}
+      </div>
+    </>
   );
 }
